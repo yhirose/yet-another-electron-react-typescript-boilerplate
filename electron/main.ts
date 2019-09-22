521a1aad6cf43310d0f5c71df438910b34fc8db9
yhirose <yuji.hirose.bug@gmail.com>
2019-09-22 01:23:45 -0400

    Add main process hot-reloading and DevTools support

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,6 @@
 import { app, ipcMain, BrowserWindow, Menu } from 'electron';
 import * as path from 'path';
+import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 
 let mainWindow: BrowserWindow | null = null;
 
@@ -17,6 +18,17 @@ function createWindow() {
   mainWindow.on('closed', () => mainWindow = null);
 
   mainWindow.loadURL('http://localhost:3000/index.html');
+
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
+    hardResetMethod: 'exit'
+  });
+
+  installExtension(REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+
+  mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', createWindow);
