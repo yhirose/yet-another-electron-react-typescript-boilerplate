62504f8e025332c595d1045ed92042fd53afb762
yhirose <yuji.hirose.bug@gmail.com>
2019-09-22 02:07:33 -0400

    Setup production package environment

--- electron/main.ts
+++ electron/main.ts
@@ -1,5 +1,6 @@
 import { app, ipcMain, BrowserWindow, Menu } from 'electron';
 import * as path from 'path';
+import * as isDev from 'electron-is-dev';
 import installExtension, { REACT_DEVELOPER_TOOLS } from "electron-devtools-installer";
 
 let mainWindow: BrowserWindow | null = null;
@@ -18,18 +19,26 @@ function createWindow() {
 
   mainWindow.on('closed', () => mainWindow = null);
 
-  mainWindow.loadURL('http://localhost:3000/index.html');
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000/index.html');
+  } else {
+    mainWindow.loadURL(`file://${__dirname}/../electron-render/index.html`);
+  }
 
-  require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
-    hardResetMethod: 'exit'
-  });
+  if (isDev) {
+    require('electron-reload')(__dirname, {
+      electron: path.join(__dirname, '..', '..', 'node_modules', '.bin', 'electron'),
+      hardResetMethod: 'exit'
+    });
+  }
 
   installExtension(REACT_DEVELOPER_TOOLS)
     .then((name) => console.log(`Added Extension:  ${name}`))
     .catch((err) => console.log('An error occurred: ', err));
 
-  mainWindow.webContents.openDevTools();
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 }
 
 app.on('ready', createWindow);
